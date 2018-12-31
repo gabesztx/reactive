726c9c6ef7eb2eb93caa6723dbfe75ba9f0d169c
gabesz <gabesztx@gmail.com>
2018-12-31 21:39:47 +0100

    form

--- src/app/creation/creation.component.ts
+++ src/app/creation/creation.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-creation',
   templateUrl: './creation.component.html',
@@ -8,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 export class CreationComponent implements OnInit {
 
   constructor() {}
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
 
 }
--- src/app/creation/from/from.component.ts
+++ src/app/creation/from/from.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-from',
@@ -6,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./from.component.scss']
 })
 export class FromComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    // Array
+    const source = from([1, 2, 3, 4, 5]);
+
+    // Promise
+    // const source = from(new Promise(resolve => {
+    //   setTimeout(() => {
+    //     resolve('Hello Promise!');
+    //   }, 2000);
+    // }));
+
+    // Map
+    // const map = new Map();
+    // map.set(1, 'Hi');
+    // map.set(2, 'Bye');
+    // const source = from(map);
+
+    const subscribe = source.subscribe(
+      val => {
+        console.log('Value: ', val);
+      },
+      err => {
+        console.log('Error: ', err);
+      },
+      () => {
+        console.log('Completed!');
+      });
+  }
 }

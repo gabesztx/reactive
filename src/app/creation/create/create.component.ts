64acfdc1823fc3e0f475a76fd1df9810ba35af7d
gabesz <gabesztx@gmail.com>
2018-12-31 23:02:47 +0100

    creation

--- src/app/creation/create/create.component.ts
+++ src/app/creation/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const numbers = [1, 2, 3, 4, 5, 7];
+    const source = Observable.create(obs => {
 
+      // basic
+      for (const n of numbers) {
+        obs.next(n);
+      }
+      obs.complete();
+
+      // interval
+      /*
+      let index = 0;
+      const timer = setInterval(() => {
+        obs.next(numbers[index]);
+        index++;
+        // error handler
+        if (numbers[index] === 2) {
+          // obs.error('Something went wrong!');
+        }
+
+        if (index === numbers.length) {
+          clearInterval(timer);
+          obs.complete();
+        }
+      }, 500);
+      */
+    });
+
+    // const source = Observable.create(observer => {
+    //   let value = 0;
+    //   const interval = setInterval(() => {
+    //     if (value % 2 === 0) {
+    //       observer.next(value);
+    //       // observer.complete();
+    //     }
+    //     value++;
+    //   }, 1000);
+    //
+    //   return () => {
+    //     console.log('clearInterval');
+    //     return clearInterval(interval);
+    //   };
+    // });
+
+    const subscribe = source.subscribe(
+      val => {
+        console.log('Value: ', val);
+      },
+      err => {
+        console.log('Error: ', err);
+      },
+      () => {
+        console.log('Completed!');
+      });
+
+    /*setTimeout(() => {
+      subscribe.unsubscribe();
+    }, 5000);*/
+  }
 }

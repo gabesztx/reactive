c498bebddc33b9118a0b1cedce8117d397e7a5c8
gabesz <gabesztx@gmail.com>
2019-01-03 23:24:31 +0100

    combinateAll

--- src/app/combination/combination.module.ts
+++ src/app/combination/combination.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CombinationComponent } from './combination.component';
 import { CombineAllComponent } from './combine-all/combine-all.component';
-import { CombineLatestComponent } from './combine-latest/combine-latest.component';
 import { ConcactComponent } from './concact/concact.component';
 import { ConcactAllComponent } from './concact-all/concact-all.component';
 
 @NgModule({
-  declarations: [CombinationComponent, CombineAllComponent, CombineLatestComponent, ConcactComponent, ConcactAllComponent],
+  declarations: [
+    CombinationComponent,
+    CombineAllComponent,
+    // ConcactComponent,
+    // ConcactAllComponent
+  ],
   imports: [
     CommonModule
   ],
--- src/app/combination/combine-all/combine-all.component.ts
+++ src/app/combination/combine-all/combine-all.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, of, timer } from 'rxjs';
+import { take, delay, tap, map, combineAll } from 'rxjs/operators';
 
 @Component({
   selector: 'app-combine-all',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CombineAllComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+
+    /*const source = interval(1000).pipe(take(2));
+    const example = source.pipe(
+      map(val => interval(1000)
+        .pipe(
+          map(i => `Result (${val}): ${i}`),
+          take(5)
+        )
+      )
+    );*/
+
+    const source1 = of(1, 2, 3);
+    const source2 = interval(1000).pipe(take(3), map(value => value + 1));
+    const source3 = of(1, 2, 3).pipe(delay(1000));
+    const combined = of(source1, source2, source3).pipe(combineAll());
+
+    const subscribe = combined.subscribe(
+      val => {
+        console.log('Emit value: ', val);
+      },
+      err => {
+        console.log('Error: ', err);
+      },
+      () => {
+        console.log('Completed!');
+      });
   }
 
 }
+
+/*
+  emit variable amount of values in a sequence and then emits a complete notification.
+*/

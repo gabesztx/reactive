d0417c1c4dc4465d1547f6e6ae5b4b551a37bac9
gabesz <gabesztx@gmail.com>
2019-01-20 15:41:32 +0100

    startwith

--- src/app/combination/startwith/startwith.component.ts
+++ src/app/combination/startwith/startwith.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { delay, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-startwith',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartwithComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+
+    const source = of(1, 2, 3).pipe(delay(2000));
+    const example = source.pipe(startWith(0));
+    const subscribe = example.subscribe(
+      val => {
+        console.log('Emit value: ', val);
+      },
+      err => {
+        console.log('Error: ', err);
+      },
+      () => {
+        console.log('Completed!');
+      });
   }
 
 }

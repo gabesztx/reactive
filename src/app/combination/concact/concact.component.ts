6e867a5f7c311e5fe2b131c2777b1950807b9bff
gabesz <gabesztx@gmail.com>
2019-02-02 00:09:09 +0100

    wip

--- src/app/combination/concact/concact.component.ts
+++ src/app/combination/concact/concact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { of, concat as concatObs } from 'rxjs';
-import { concat, delay } from 'rxjs/operators';
+import { of, concat, timer } from 'rxjs';
+import { combineAll, concatAll, delay } from 'rxjs/operators';
 
 
 @Component({
@@ -14,13 +14,12 @@ export class ConcactComponent implements OnInit {
   }
 
   ngOnInit() {
-    const source1$ = of(1, 2, 3).pipe(delay(1000));
-    const source2$ = of(4, 5, 6).pipe(delay(1000));
-    const source3$ = of(7, 8, 9).pipe(delay(2000));
-    const source4$ = of(10, 11, 12);
-
-    const example$ = concatObs(source1$, source2$, source3$, source4$);
-
+    const source1$ = of(1, 2, 3);
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
+    // const source4$ = of(10, 11, 12);
+    const source4$ = of(10, 11, 12).pipe(delay(3000));
+    const example$ = concat(source1$, source2$, source3$, source4$);
     const subscribe = example$.subscribe(
       val => {
         console.log('Emit value: ', val);

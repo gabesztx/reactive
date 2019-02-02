46396a6151f23c89a05cea6257ff9346177b9df1
gabesz <gabesztx@gmail.com>
2019-02-02 02:14:51 +0100

    wip

--- src/app/combination/combine-latest/combine-latest.component.ts
+++ src/app/combination/combine-latest/combine-latest.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timer, combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-combine-latest',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CombineLatestComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    const timerOne = timer(1000, 4000);
+    const timerTwo = timer(2000, 4000);
+    const timerThree = timer(3000, 4000);
+
+    const example$ = combineLatest(timerOne, timerTwo, timerThree);
+
+    const subscribe = example$.subscribe(
+      val => {
+        console.log('Emit value: ', val);
+      },
+      err => {
+        console.log('Error: ', err);
+      },
+      () => {
+        console.log('Completed!');
+      });
   }
 
 }

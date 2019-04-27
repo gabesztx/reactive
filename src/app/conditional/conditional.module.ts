0d119b8af91bf3ef062e3af2ba5e2e7bde8d9a35
gabesz <gabesztx@gmail.com>
2019-04-27 06:17:14 +0200

    new

--- src/app/conditional/conditional.module.ts
+++ src/app/conditional/conditional.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConditionalComponent } from './conditional.component';
 import { IfelseComponent } from './ifelse/ifelse.component';
+import { SequenceEqualComponent } from './sequence-equal/sequence-equal.component';
 
 @NgModule({
-  declarations: [ConditionalComponent, IfelseComponent],
+  declarations: [ConditionalComponent, IfelseComponent, SequenceEqualComponent],
   imports: [
     CommonModule
   ],
---
+++ src/app/conditional/sequence-equal/sequence-equal.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { from, fromEvent } from 'rxjs';
+import { sequenceEqual, map, bufferCount, mergeMap, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-sequence-equal',
+  templateUrl: './sequence-equal.component.html',
+  styleUrls: ['./sequence-equal.component.scss']
+})
+export class SequenceEqualComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    const expectedSequence = from(['q', 'w', 'e']);
+    const keyStream = fromEvent(document, 'keydown')
+      .pipe(
+        map((e: KeyboardEvent) => e.key),
+        bufferCount(3), // 3 trigger után inditja el a streamet majd az értékeket tömbe adja vissza
+        tap(v => console.log('Buffer', v)),
+        mergeMap(keyDowns =>
+          from(keyDowns)
+            .pipe(
+              tap(v => console.log('KeyDodwns:', v)),
+              sequenceEqual(expectedSequence),
+              // tap(isItQwerty => console.log('Log 2', isItQwerty ? 'WELL DONE!' : 'TYPE AGAIN!'))
+            )
+        )
+      );
+
+    const subscribe = keyStream.subscribe(
+      val => {
+        console.log('Emit: ', val);
+      },
+      err => {
+        console.log('Error: ', err);
+      },
+      () => {
+        console.log('Completed!');
+      });
+  }
+
+}
